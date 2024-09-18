209960948acd97306a4f482a9c01e1e65ffd0186
christopher <voltaire.christopher@yahoo.com>
2024-09-18 16:43:20 -0400

    first save

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -18,5 +18,20 @@ router.get("/api/csrf/restore", (req, res) => {
     });
 });
 // ...
+// backend/routes/index.js
+// ...
+const apiRouter = require('./api');
+
+router.use('/api', apiRouter);
+// ...
+
+// backend/routes/api/index.js
+// ...
+
+router.post('/test', function(req, res) {
+    res.json({ requestBody: req.body });
+  });
+
+  // ...
 
 module.exports = router;
