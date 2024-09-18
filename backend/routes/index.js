ea1a56dd944040cb666636c7af6559c7e39a4d56
christopher <voltaire.christopher@yahoo.com>
2024-09-18 18:01:36 -0400

    working on test

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,5 +1,5 @@
 // backend/routes/index.js
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // router.get('/hello/world', function (req, res) {
@@ -11,27 +11,17 @@ const router = express.Router();
 // ...
 // Add a XSRF-TOKEN cookie
 router.get("/api/csrf/restore", (req, res) => {
-    const csrfToken = req.csrfToken();
-    res.cookie("XSRF-TOKEN", csrfToken);
-    res.status(200).json({
-        'XSRF-Token': csrfToken
-    });
+  const csrfToken = req.csrfToken();
+  res.cookie("XSRF-TOKEN", csrfToken);
+  res.status(200).json({
+    "XSRF-Token": csrfToken,
+  });
 });
-// ...
 // backend/routes/index.js
 // ...
 const apiRouter = require('./api');
 
 router.use('/api', apiRouter);
 // ...
-
-// backend/routes/api/index.js
-// ...
-
-router.post('/test', function(req, res) {
-    res.json({ requestBody: req.body });
-  });
-
-  // ...
 
 module.exports = router;
