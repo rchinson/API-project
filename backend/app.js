e91ec77a8d922fa302b6102bc3a1d682a3ad6d33
christopher <voltaire.christopher@yahoo.com>
2024-09-18 19:30:09 -0400

    phase 2 finished

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,7 @@ const cookieParser = require('cookie-parser');
 
 const { environment } = require('./config');
 const isProduction = environment === 'production';
-
+const { ValidationError } = require('sequelize');
 const routes = require('./routes');
 const app = express();
 
@@ -46,6 +46,40 @@ app.use(
 
 app.use(routes); // Connect all the routes
 
+
+// backend/app.js
+// ...
+// Catch unhandled requests and forward to error handler.
+app.use((_req, _res, next) => {
+    const err = new Error("The requested resource couldn't be found.");
+    err.title = "Resource Not Found";
+    err.errors = { message: "The requested resource couldn't be found." };
+    err.status = 404;
+    next(err);
+  });
+
+  app.use((err, _req, _res, next) => {
+    // check if error is a Sequelize error:
+    if (err instanceof ValidationError) {
+      let errors = {};
+      for (let error of err.errors) {
+        errors[error.path] = error.message;
+      }
+      err.title = 'Validation error';
+      err.errors = errors;
+    }
+    next(err);
+  });
+  app.use((err, _req, res, _next) => {
+    res.status(err.status || 500);
+    console.error(err);
+    res.json({
+      title: err.title || 'Server Error',
+      message: err.message,
+      errors: err.errors,
+      stack: isProduction ? null : err.stack
+    });
+  });
 
 
 // backend/app.js
