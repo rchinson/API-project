adf0293f353fcf9767b83b54f096bfc6b57ef3c1
rchinson <rhinson2010@gmail.com>
2024-09-19 14:56:09 -0700

    added seed migration model files

--- backend/db/migrations/20240919212105-create-user.js
+++ backend/db/migrations/20240919212105-create-user.js
@@ -1,9 +1,10 @@
 'use strict';
 
-const { DataTypes } = require('sequelize');
-const { now } = require('sequelize/lib/utils');
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
 
-/** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable('Users', {
@@ -14,38 +15,34 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       username: {
-        type: Sequelize.STRING,
+        type: Sequelize.STRING(30),
         allowNull: false,
-        unique: true,
-        validate: {
-          max: 30
-        }
+        unique: true
       },
       email: {
-        type: Sequelize.STRING,
+        type: Sequelize.STRING(256),
         allowNull: false,
-        unique: true,
-        validate: {
-          max: 256
-        }
+        unique: true
       },
       hashedPassword: {
-        type: Sequelize.STRING,
+        type: Sequelize.STRING.BINARY,
         allowNull: false
       },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE,
-        defaultValue: DataTypes.NOW()
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
       },
       updatedAt: {
         allowNull: false,
         type: Sequelize.DATE,
-        defaultValue: DataTypes.NOW()
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
       }
-    });
+    }, options);
   },
+
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Users');
+    options.tableName = "Users";
+    return queryInterface.dropTable(options);
   }
 };
