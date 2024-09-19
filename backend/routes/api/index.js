8c91e79226ee1a566908f056e24ab1b98632244f
christopher <voltaire.christopher@yahoo.com>
2024-09-19 19:36:40 -0400

    phase 4 almost complete

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -3,7 +3,8 @@ const router = require('express').Router();
 
 // backend/routes/api/index.js
 // ...
-
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
 const { setTokenCookie } = require('../../utils/auth.js');
 const { User } = require('../../db/models');
 
@@ -13,6 +14,12 @@ const { requireAuth } = require('../../utils/auth.js');
 
 
 router.use(restoreUser);
+
+
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
+
 
 router.get(
   '/restore-user',
---
+++ backend/routes/api/session.js
@@ -0,0 +1,51 @@
+// backend/routes/api/session.js
+const express = require("express");
+const { Op } = require("sequelize");
+const bcrypt = require("bcryptjs");
+
+const { setTokenCookie, restoreUser } = require("../../utils/auth");
+const { User } = require("../../db/models");
+
+const router = express.Router();
+router.post("/", async (req, res, next) => {
+  const { credential, password } = req.body;
+
+  const user = await User.unscoped().findOne({
+    where: {
+      [Op.or]: {
+        username: credential,
+        email: credential,
+      },
+    },
+  });
+
+  if (!user || !bcrypt.compareSync(password, user.hashedPassword.toString())) {
+    const err = new Error("Login failed");
+    err.status = 401;
+    err.title = "Login failed";
+    err.errors = { credential: "The provided credentials were invalid." };
+    return next(err);
+  }
+
+  const safeUser = {
+    id: user.id,
+    email: user.email,
+    username: user.username,
+  };
+
+  await setTokenCookie(res, safeUser);
+
+
+
+  return res.json({
+    user: safeUser,
+  });
+});
+
+ // Log out
+ router.delete("/", (_req, res) => {
+  res.clearCookie("token");
+  return res.json({ message: "success" });
+});
+
+module.exports = router;
