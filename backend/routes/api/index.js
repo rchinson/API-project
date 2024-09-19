33f184df6ab3096e66be38f64faa627f173efd99
rchinson <rhinson2010@gmail.com>
2024-09-19 15:49:44 -0700

    finished phase 3

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,11 +1,49 @@
 // backend/routes/api/index.js
 const router = require('express').Router();
+
 // backend/routes/api/index.js
 // ...
+
+const { setTokenCookie } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
+
+const { restoreUser } = require('../../utils/auth.js');
+
+const { requireAuth } = require('../../utils/auth.js');
+
+
+router.use(restoreUser);
+
+router.get(
+  '/restore-user',
+  (req, res) => {
+    return res.json(req.user);
+  }
+);
+
+router.get(
+  '/require-auth',
+  requireAuth,
+  (req, res) => {
+    return res.json(req.user);
+  }
+);
+
+router.get('/set-token-cookie', async (_req, res) => {
+  const user = await User.findOne({
+    where: {
+      username: 'Demo-lition'
+    }
+  });
+  setTokenCookie(res, user);
+  return res.json({ user: user });
+});
+
 
 router.post('/test', function(req, res) {
     res.json({ requestBody: req.body });
   });
+
 
   // ...
 module.exports = router;
