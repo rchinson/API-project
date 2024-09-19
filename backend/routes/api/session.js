1ba1f5bc7fca4eea0c6dbde9924e225cf00feb52
christopher <voltaire.christopher@yahoo.com>
2024-09-19 19:57:15 -0400

    phase 4 done

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -42,6 +42,29 @@ router.post("/", async (req, res, next) => {
   });
 });
 
+router.get(
+  '/',
+  (req, res) => {
+    const { user } = req;
+    if (user) {
+      const safeUser = {
+        id: user.id,
+        email: user.email,
+        username: user.username,
+      };
+      return res.json({
+        user: safeUser
+      });
+    } else return res.json({ user: null });
+  }
+);
+
+
+
+
+
+
+
  // Log out
  router.delete("/", (_req, res) => {
   res.clearCookie("token");
