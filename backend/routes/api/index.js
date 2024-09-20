c5ba825064f9f5507b4b05f977db561fb2ce6eaf
christopher <voltaire.christopher@yahoo.com>
2024-09-20 19:40:22 -0400

    draft

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,58 +1,44 @@
 // backend/routes/api/index.js
-const router = require('express').Router();
+const router = require("express").Router();
 
 // backend/routes/api/index.js
 // ...
-const sessionRouter = require('./session.js');
-const usersRouter = require('./users.js');
-const { setTokenCookie } = require('../../utils/auth.js');
-const { User } = require('../../db/models');
+const sessionRouter = require("./session.js");
+const usersRouter = require("./users.js");
+const { setTokenCookie } = require("../../utils/auth.js");
+const { User } = require("../../db/models");
 
-const { restoreUser } = require('../../utils/auth.js');
+const { restoreUser } = require("../../utils/auth.js");
 
-const { requireAuth } = require('../../utils/auth.js');
-const apiRouter = require('./api');
-
-router.use('/api', apiRouter);
+const { requireAuth } = require("../../utils/auth.js");
 
 router.use(restoreUser);
 
+router.use("/session", sessionRouter);
 
-router.use('/session', sessionRouter);
+router.use("/users", usersRouter);
 
-router.use('/users', usersRouter);
+router.get("/restore-user", (req, res) => {
+  return res.json(req.user);
+});
 
+router.get("/require-auth", requireAuth, (req, res) => {
+  return res.json(req.user);
+});
 
-router.get(
-  '/restore-user',
-  (req, res) => {
-    return res.json(req.user);
-  }
-);
-
-router.get(
-  '/require-auth',
-  requireAuth,
-  (req, res) => {
-    return res.json(req.user);
-  }
-);
-
-router.get('/set-token-cookie', async (_req, res) => {
+router.get("/set-token-cookie", async (_req, res) => {
   const user = await User.findOne({
     where: {
-      username: 'Demo-lition'
-    }
+      username: "Demo-lition",
+    },
   });
   setTokenCookie(res, user);
   return res.json({ user: user });
 });
 
+router.post("/test", function (req, res) {
+  res.json({ requestBody: req.body });
+});
 
-router.post('/test', function(req, res) {
-    res.json({ requestBody: req.body });
-  });
-
-
-  // ...
+// ...
 module.exports = router;
