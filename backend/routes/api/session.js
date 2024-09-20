6cdaf61a98f5e43b4d5ae306077de279d4be019f
rchinson <rhinson2010@gmail.com>
2024-09-20 14:03:34 -0700

    finished phase 5

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -53,6 +53,8 @@ router.post(
 
     const safeUser = {
       id: user.id,
+      firstName: user.firstName,
+      lastName: user.lastName,
       email: user.email,
       username: user.username,
     };
--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -37,9 +37,9 @@ router.post(
     '/',
     validateSignup,
     async (req, res) => {
-      const { email, password, username } = req.body;
+      const { firstName, lastName, email, password, username } = req.body;
       const hashedPassword = bcrypt.hashSync(password);
-      const user = await User.create({ email, username, hashedPassword });
+      const user = await User.create({ firstName, lastName, email, username, hashedPassword });
 
       const safeUser = {
         id: user.id,
