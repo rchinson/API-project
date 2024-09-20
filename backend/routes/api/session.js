ab1a831eb64f97c3a32426dc6da7b79a2bb61f6c
rchinson <rhinson2010@gmail.com>
2024-09-20 10:53:16 -0700

    Add User input validation on user login backend endpoint

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -2,44 +2,68 @@
 const express = require("express");
 const { Op } = require("sequelize");
 const bcrypt = require("bcryptjs");
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
 
 const { setTokenCookie, restoreUser } = require("../../utils/auth");
 const { User } = require("../../db/models");
 
+
+
 const router = express.Router();
-router.post("/", async (req, res, next) => {
-  const { credential, password } = req.body;
 
-  const user = await User.unscoped().findOne({
-    where: {
-      [Op.or]: {
-        username: credential,
-        email: credential,
+
+
+const validateLogin = [
+  check('credential')
+    .exists({ checkFalsy: true })
+    .notEmpty()
+    .withMessage('Please provide a valid email or username.'),
+  check('password')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a password.'),
+  handleValidationErrors
+];
+
+
+
+
+router.post(
+  "/", 
+  validateLogin,
+  async (req, res, next) => {
+    const { credential, password } = req.body;
+
+    const user = await User.unscoped().findOne({
+      where: {
+        [Op.or]: {
+          username: credential,
+          email: credential,
+        },
       },
-    },
-  });
+    });
 
-  if (!user || !bcrypt.compareSync(password, user.hashedPassword.toString())) {
-    const err = new Error("Login failed");
-    err.status = 401;
-    err.title = "Login failed";
-    err.errors = { credential: "The provided credentials were invalid." };
-    return next(err);
-  }
+    if (!user || !bcrypt.compareSync(password, user.hashedPassword.toString())) {
+      const err = new Error("Login failed");
+      err.status = 401;
+      err.title = "Login failed";
+      err.errors = { credential: "The provided credentials were invalid." };
+      return next(err);
+    }
 
-  const safeUser = {
-    id: user.id,
-    email: user.email,
-    username: user.username,
-  };
+    const safeUser = {
+      id: user.id,
+      email: user.email,
+      username: user.username,
+    };
 
-  await setTokenCookie(res, safeUser);
-
+    await setTokenCookie(res, safeUser);
 
 
-  return res.json({
-    user: safeUser,
-  });
+
+    return res.json({
+      user: safeUser,
+    });
 });
 
 router.get(
